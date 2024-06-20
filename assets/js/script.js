3b2d5c00ad7d8ebbef13f1b86770794183aa914a
Marlon Santos <xsantos.marlonx@gmail.com>
2024-06-20 11:42:43 -0300

    Project correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,83 @@
-const hameburger = document.querySelector('.hamburger');
-const listMenu = document.querySelector('ul');
+let modalQt = 1;
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
 
-hameburger.addEventListener('click', ()=>{
-    hameburger.classList.toggle('active');
-    listMenu.classList.toggle('active');
+pizzaJson.map((item, index) => {
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
+    pizzaItem.setAttribute('data-key', index);
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price[2].toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (event) => {
+        event.preventDefault();
+        let key = event.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
+
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price[2].toFixed(2)}`;
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if (sizeIndex == 2) {
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
+
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(() => {
+            c('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+    });
+
+    c('.pizza-area').append(pizzaItem);
 });
 
-const link = document.querySelectorAll('ul a').forEach(element =>{
-    element.addEventListener('click', ()=>{
-        hameburger.classList.remove('active');
-        listMenu.classList.remove('active');
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', closeModal);
+});
+
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if (modalQt > 1) {
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+
+});
+
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    size.addEventListener('click', (e) => {
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+
+
+
+        if (e.target == c('#size-zero')) {
+            c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[sizeIndex].price[0].toFixed(2)}`;
+        }
+        else if(e.target == c('#size-one')){
+            c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[sizeIndex].price[1].toFixed(2)}`;
+        }
+        else{
+            c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[sizeIndex].price[2].toFixed(2)}`;
+        }
     });
 });
