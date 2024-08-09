50eafceb2f58f586697c0ecb397cdf80f61a8f19
Marlon Santos <xsantos.marlonx@gmail.com>
2024-08-09 10:51:42 -0300

    Fixed MIME type error, files renamed and separated by folders

--- assets/js/pizzas.js
+++ assets/js/pizzas.js
@@ -1,9 +1,9 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'assets/images/pizza.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'assets/images/pizza2.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'assets/images/pizza3.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Americana', img:'assets/images/pizza4.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Portuguesa', img:'assets/images/pizza5.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Moda da Casa', img:'assets/images/pizza6.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Frango Catupiry', img:'assets/images/pizza.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+    {id:1, name:'Mussarela', img:'/assets/images/pizza.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'/assets/images/pizza2.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'/assets/images/pizza3.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'/assets/images/pizza4.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Portuguesa', img:'/assets/images/pizza5.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'/assets/images/pizza6.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Frango Catupiry', img:'/assets/images/pizza.png', price:[27.99, 32.50, 40.00], sizes:['100g', '320g', '530g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
 ];
